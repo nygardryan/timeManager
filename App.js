3f5c6723da509e68232dcf3d1718f23d99efd2ea
Ryan Nygard <nygardryan@yahoo.com>
2019-03-22 17:59:01 -0600

    Got AsyncStorage working

--- App.js
+++ App.js
@@ -1,26 +1,111 @@
 import React, { Component } from 'react';
-import AsyncStorageExample from './async_storage_tool.js'
+// import {AsyncStorageExample, addToDatabase, getFromDatabase } from './async_storage_tool.js'
+var data = require('./async_storage_tool.js');
 //Elements
 import {
+  AppRegistry,
   Platform,
   StyleSheet,
   Text,
   View,
+  Button,
+  AsyncStorage
 } from 'react-native';
 //Screens
 import Settings from './screens/Settings';
 import Home from './screens/Home';
 
 
+displayData = async (key) => {
+  try {
+    let value = AsyncStorage.getItem(key);
+    alert(value);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-export default class Main extends Component {
+function hello() {
+  console.log("hello");
+}
+
+function getData(key) {
+  console.log(key);
+}   // try {
+    //  return await AsyncStorage.getItem(key);
+    // } catch (err) {
+    //  console.log(err);
+    // }
+
+data.addToDatabase("you", "two");
+
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tickets: 'waiting'};
+    var that = this;
+    data.getFromDatabase('you', (err, result)=>{
+      this.setState(previousState => (
+          { tickets: result}
+          ))
+      console.log("---------------------------------");
+      console.log(this);
+    });
+  }
+
   render() {
+    let textValue = typeof(data.getFromDatabase('you'));
+    console.log(textValue);
     return (
-      <AsyncStorageExample />
+      <View style={{flex:1}}>
+        <View style={{height: 25, backgroundColor: 'gray'}}></View>
+        <Text style={styles.top}>Monday</Text>
+        <Text>{this.state.tickets}</Text>
+        <Text>Hey Norm</Text>
+      </View>
     );
   }
 }
 
+class AddTask extends Component {
+  render() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <Button
+          onPress={hello}
+          title="Add New Task"
+         />
+         <Button
+          onPress={hello}
+          title="Start Pomodora Timer"
+         />
+      </View>
+    );
+  }
+}
+
+export default class Main extends Component {
+
+  render() {
+    return (
+      <View>
+        <TopBar />
+        <AddTask />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  top: {
+    fontWeight: 'bold',
+    fontSize: 45,
+  },
+  add: {
+
+  },
+});
 
 // const App = () => {
 //   return (
--- async_storage_tool.js
+++ async_storage_tool.js
@@ -2,40 +2,19 @@ import React, { Component } from 'react'
 import { StatusBar } from 'react-native'
 import { AsyncStorage, Text, View, TextInput, StyleSheet } from 'react-native'
 
-class AsyncStorageExample extends Component {
-   state = {
-      'name': ''
-   }
-   componentDidMount = () => AsyncStorage.getItem('name').then((value) => this.setState({ 'name': value }))
-   
-   setName = (value) => {
-      AsyncStorage.setItem('name', value);
-      this.setState({ 'name': value });
-   }
-   render() {
-      return (
-         <View style = {styles.container}>
-            <TextInput style = {styles.textInput} autoCapitalize = 'none'
-            onChangeText = {this.setName}/>
-            <Text>
-               {this.state.name}
-            </Text>
-         </View>
-      )
-   }
-}
-export default AsyncStorageExample
+var methods ={
 
-const styles = StyleSheet.create ({
-   container: {
-      flex: 1,
-      alignItems: 'center',
-      marginTop: 50
-   },
-   textInput: {
-      margin: 5,
-      height: 100,
-      borderWidth: 1,
-      backgroundColor: '#7685ed'
-   }
-})
+	addToDatabase: function(key, value) {
+		AsyncStorage.setItem(key, JSON.stringify(value));
+	},
+
+	getFromDatabase: async (key, callback) => {
+		try {
+			AsyncStorage.getItem(key, callback);
+		} catch (err) {
+			console.log(err);
+		}
+	},
+};
+
+module.exports = methods;
